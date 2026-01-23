3ac604a662be544e9e30002451c51340a9855125
Zain-Mrza <zzmirzazz@aol.com>
2026-01-22 21:19:52 -0800

    Added comments for routes

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,14 +4,26 @@ api = Blueprint("api", __name__)
 
 @api.route("/health", methods=["GET"])
 def health_check():
+    """
+    Checks if Flask server is working.
+    """
+
     return jsonify({"status": "ok"}), 200
 
 
 @api.route("/submit", methods=["POST"])
 def submit_measurements():
+    """ 
+    Links the frontend to our backend.
+    Input from the frontend is recieved, ran through our algorithm and sent back to the frontend.
+    This is NOT complete.
+    """
+
     data = request.json
 
-    # Placeholder logic
+    # Algorithm goes here
+
+    # Algorithm result is sent back to frontend
     response = {
         "risk_level": "unknown",
         "recommendation": "This is a placeholder response",
