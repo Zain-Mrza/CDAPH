b2ec27150a33cabaf0311d441dd8ac2f82a51c5e
Zain-Mrza <zzmirzazz@aol.com>
2026-01-25 20:34:51 -0800

    More descriptive names

--- backend/state.py
+++ backend/state.py
@@ -1,7 +1,7 @@
 patient_state = {
     "blood_pressure": None,
-    "height": None,
-    "weight": None,
-    "waist": None,
+    "height_cm": None,
+    "weight_kg": None,
+    "waist_cm": None,
     "visual_acuity": None
 }
