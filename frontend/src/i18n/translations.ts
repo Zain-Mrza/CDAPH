81473e0db4ac151279908bde96eeb4f78e36c270
Zain-Mrza <zzmirzazz@aol.com>
2026-01-26 19:39:27 -0800

    Established initial translation logic (dictionary-based)

--- frontend/src/i18n/translations.ts
+++ frontend/src/i18n/translations.ts
@@ -1,2 +1,24 @@
-// export type Language = "en" | "es";
+/**
+* DEMO FOR TRANSLATION STORAGE
+* Needs to be extended to every page we plan to add
+* Call it via 
+* TODO: Find some way to neatly organize translation files
+*/
 
+export const translations = {
+    en: {
+        start: "Start",
+        next: "Next",
+        back: "Back",
+        bloodPressure: "Blood Pressure",
+        instructionsBP: "Place your arm inside the cuff and remain still.",
+    },
+
+    es: {
+        start: "Comenzar",
+        next: "Siguiente",
+        back: "Atrás",
+        bloodPressure: "Presión arterial",
+        instructionsBP: "Coloque su brazo dentro del brazalete y permanezca quieto."
+    }
+} as const; // Literal typing, guarantees values exist for keys
