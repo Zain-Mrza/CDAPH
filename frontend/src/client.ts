c39b4a01e8a5d080550234dd6197e01d6ed208f3
Zain-Mrza <zzmirzazz@aol.com>
2026-01-26 21:23:24 -0800

    Create initial routing from React frontend to Flask backend

---
+++ frontend/src/client.ts
@@ -0,0 +1,37 @@
+export interface SubmitResponse { // defines what shape SubmitResponse should be
+    status: string;
+}
+
+export type MeasurementType =
+    | "bp"
+    | "height"
+    | "weight"
+    | "waist"
+    | "visual_acuity";
+
+
+export async function healthCheck() {
+    const res = await fetch("/api/health");
+    return res.json();
+}
+
+/**
+ * Function to submit all measurements to api/measurement endpoint (backend)
+ * 
+ * @param type - MeasurementType. Must be strings as seen above.
+ * @param value - Value sent to backend. For example, you send 55 for weight in kg.
+ * @returns - JSON response confirming status that the "type" measurement was stored for confirmation.
+ */
+export async function submitMeasurements(
+    type: MeasurementType,
+    value: string | number
+): Promise<SubmitResponse> {
+
+    const res = await fetch("api/measurement", {
+        method: "POST",
+        headers: { "Content-Type": "application.json" },
+        body: JSON.stringify({ type, value }),
+
+    });
+    return res.json();
+}
