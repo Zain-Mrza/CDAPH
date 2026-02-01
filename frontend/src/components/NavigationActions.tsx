1695f2e7a03cd95cffe84a374b08ea717e86aede
Zain-Mrza <zzmirzazz@aol.com>
2026-02-01 02:04:47 -0800

    Centralized button contruction here

---
+++ frontend/src/components/NavigationActions.tsx
@@ -0,0 +1,34 @@
+type Props = {
+    clickBack?: () => void;
+    clickNext?: () => void;
+    disableNext?: boolean;
+    nextLabel?: string;
+};
+export default function NavigationActions({
+    clickBack,
+    clickNext,
+    disableNext,
+    nextLabel = "Continue",
+}: Props) {
+    return (
+        <div className="actions">
+            {clickBack ? (
+                <button className="button secondary" onClick={clickBack}>
+                    Back
+                </button>
+            ) : (
+                <div />
+            )}
+            ;
+            {clickNext && (
+                <button
+                    className="button"
+                    onClick={clickNext}
+                    disabled={disableNext}
+                >
+                    {nextLabel}
+                </button>
+            )}
+        </div>
+    );
+}
