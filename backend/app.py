ea35df4e0b8b9937a549d34a69ceb392b45d0128
Zain-Mrza <zzmirzazz@aol.com>
2026-02-10 18:23:44 -0800

    Changed file structure

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask
-from api import api
+from routes import api
+
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(api)
     return app
+
 
 if __name__ == "__main__":
     create_app().run(debug=True)
