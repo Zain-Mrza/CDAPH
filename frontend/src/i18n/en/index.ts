7a6a32c33ddcca8ce136a1d995e7842cac3ecc66
Zain-Mrza <zzmirzazz@aol.com>
2026-02-22 22:18:23 -0800

    Added indices for import behavior

---
+++ frontend/src/i18n/en/index.ts
@@ -0,0 +1,5 @@
+export { Instructions } from "./Instructions";
+export { InputInstructions as measurementInput } from "./measurementInput";
+export { BLOOD_PRESSURE_MESSAGES as bloodPressureResults } from "./bloodPressureResults";
+export { SummaryTranslations as summary } from "./summary";
+export { NavigationText } from "./navigationActions";
