8a7875252829360a687281f1863f08907a1866e7
Zain-Mrza <zzmirzazz@aol.com>
2026-02-22 22:17:56 -0800

    Added types so seperate folders stay type safe

---
+++ frontend/src/i18n/types.ts
@@ -0,0 +1,65 @@
+export type language = "en" | "es";
+
+export interface MeasurementInputTypes {
+    ageSex: {
+        title: string;
+        instruction: string;
+        label1: string;
+        label2: string;
+        helper1: string;
+        helper2: string;
+        helper3: string;
+        helper4: string;
+        helper5: string;
+        error1: string;
+        error2: string;
+        error3: string;
+        error4: string;
+    };
+
+    bp: {
+        title: string;
+        instruction: string;
+        label1: string;
+        label2: string;
+        helper1: string;
+        helper2: string;
+        error1: string;
+        error2: string;
+    };
+
+    height: {
+        title: string;
+        instruction: string;
+        label1: string;
+    };
+
+    weight: {
+        title: string;
+        instruction: string;
+        label1: string;
+    };
+}
+
+export interface NavigationTypes {
+    next: string;
+    back: string;
+    skipSurvey: string;
+}
+
+export interface InstructionTypes {
+    bpInstructions: {
+        thanks: string;
+        instruction: string;
+    };
+
+    heightInstructions: {
+        thanks: string;
+        instruction: string;
+    };
+
+    weightInstructions: {
+        thanks: string;
+        instruction: string;
+    };
+}
